4251daaf1e3d81526c62ba883f5e22ee706d8e05
lss555 <chrisheibel7@gmail.com>
2023-03-09 14:29:56 -0700

    blog cards not working as grid

--- src/components/blog/blog-styles.js
+++ src/components/blog/blog-styles.js
@@ -1,51 +1,106 @@
 import styled from 'styled-components';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-export const BlogContainer = styled.div`
-	margin-top 120px;
-`;
+// export const BlogContainer = styled.div`
+// 	margin-top 120px;
 
-export const BlogHeader = styled.h1`
-	margin-left: 2rem;
-	margin-right: 2rem;
-	height: 100px;
-	line-height: 80px;
-	border-bottom: 1px solid grey;
+// `;
+
+// export const BlogHeader = styled.h1`
+// 	margin-left: 2rem;
+// 	margin-right: 2rem;
+// 	height: 100px;
+// 	line-height: 80px;
+// 	border-bottom: 1px solid grey;
 	
-`;
+// `;
 
-export const BlogCardContainer = styled(Link)`
-	display: flex;
-	padding-right: 38px;
-	padding-left: 38px;
-	text-decoration: none;
-	color: black;
+// export const BlogCardContainer = styled(Link)`
+// 	display: flex;
+// 	padding-right: 38px;
+// 	padding-left: 38px;
+// 	text-decoration: none;
+// 	color: black;
+// 	height: 100%;
+// 	border: 3px solid red;
+
+// `;
+
+// export const BlogCards = styled.div`
+// 	margin: 100px auto auto auto;
+// 	padding-right: 1rem
+// 	padding-left: 1rem;
+// 	max-width: 2000px;
+// 	-webkit-column-count: 3;
+// 	-moz-column-count: 3;
+// 	column-count: 3;
+// 	-webkit-column-width: 33%;
+// 	-moz-column-width: 33%;
+// 	column-width: 33%;
+// 	margin-left: auto;
+// 	margin-right: auto;
+// 	width: 100%;
 	
-`;
+// `;
 
-export const BlogCards = styled.div`
+// export const BlogCardImage = styled.img`
+// 	object-fit: cover;
+// 	height: 456px;
+// 	width: 361px;
+// `;
+
+// export const BlogPostedDate = styled.p`
+// 	font-size: 12px;
+// `;
+
+// export const BlogSneakPeakText = styled.p`
+// 	width: 361px;
+
+// `;
+
+// export const BlogPostTitle = styled.h2`
+// 	margin-bottom: 0;
+// `;
+
+// New Card
+
+export const CardContainer = styled.div`
+	overflow: hidden;
+	box-shadow: 0 2px 20px gray;
+	border-radius: 5px;
 	display: flex;
-	margin: 100px auto auto auto;
-	padding-right: 1rem
-	padding-left: 1rem;
-	justify-content: center;
-
+	flex-direction: column;
+	cursor: pointer;
+	justify-content: space-between;
+	transition: transform 200ms ease-in;
 `;
 
-export const BlogCardImage = styled.img`
+export const CardBody = styled.div`
+	margin: 2rem;
+`;
+
+export const CardImage = styled.img`
 	object-fit: cover;
 	height: 456px;
 	width: 361px;
 `;
 
-export const BlogPostedDate = styled.p`
-	font-size: 12px;
+export const CardTitle = styled.h2`
+
 `;
 
-export const BlogSneakPeakText = styled.p`
-	width: 361px;
+export const CardDescription = styled.p`
+
 `;
 
-export const BlogPostTitle = styled.h2`
-	margin-bottom: 0;
+export const BlogWrapper = styled.div`
+	display: grid;
+	width: 100%;
+	grid-template-columns: 1fr 1fr 1fr;
+	gap: 2rem;
+	justify-content: center;
+`;
+
+export const CardButton = styled.button`
+
 `;
--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import sanityClient from "../../client.js";
 import imageUrlBuilder from "@sanity/image-url";
-import { BlogContainer, BlogHeader, BlogCardContainer, BlogCards, BlogCardImage, BlogPostedDate, BlogSneakPeakText, BlogPostTitle } from './blog-styles.js';
+import { CardContainer, CardBody, CardImage, CardTitle, CardDescription, CardButton, BlogWrapper } from './blog-styles.js';
+// BlogContainer, BlogHeader, BlogCardContainer, BlogCards, BlogCardImage, BlogPostedDate, BlogSneakPeakText, BlogPostTitle
 
-
-const Blog = () => {
+function Card() {
 const [allPostsData, setAllPosts] = useState(null);
 
 const builder = imageUrlBuilder(sanityClient);
@@ -33,7 +33,60 @@ function urlFor(source) {
   }, []);
 
 	return (
-		<BlogContainer>
+		<div>
+			{allPostsData &&
+				allPostsData.map((post, index) => (
+					<CardContainer key={post.slug.current}>
+						<CardBody>
+							<CardImage src={urlFor(post.mainImage.asset.url).width(1200).url()} alt="Blog post"/>
+							<CardTitle>{post.title}</CardTitle>
+							<CardDescription>add 1 to 2 sentence snippet about blog post</CardDescription>
+							<CardButton>View Post</CardButton>
+						</CardBody>
+					</CardContainer>
+					))}
+		</div>
+		)
+}
+
+const Blog = () => {
+// const [allPostsData, setAllPosts] = useState(null);
+
+// const builder = imageUrlBuilder(sanityClient);
+// function urlFor(source) {
+//   return builder.image(source);
+// }
+
+//   useEffect(() => {
+//     sanityClient
+//       .fetch(
+//         `*[_type == "post"]{
+//         title,
+//         slug,
+//         body,
+//         publishedAt,
+//         mainImage{
+//           asset->{
+//           _id,
+//           url
+//         }
+//       }
+//     }`
+//       )
+//       .then((data) => setAllPosts(data))
+//       .catch(console.error);
+//   }, []);
+
+	return (
+		<BlogWrapper>
+			<Card />
+		</BlogWrapper>
+		)
+}
+
+export default Blog;
+
+{/*<BlogContainer>
 			<BlogHeader>SOAR Blog</BlogHeader>
 			<BlogCards>
 				{allPostsData &&
@@ -43,15 +96,11 @@ function urlFor(source) {
 								<BlogCardImage src={urlFor(post.mainImage.asset.url).width(1200).url()} alt="Blog post"/>
 								<span>
 									<BlogPostTitle>{post.title}</BlogPostTitle>
-									<BlogPostedDate>Published on {post.publishedAt}</BlogPostedDate>
+									<BlogPostedDate>{post.publishedAt}</BlogPostedDate>
 									<BlogSneakPeakText>sneak peak text sneak peak text sneak peak text sneak peak text sneak peak text sneak peak text v sneak peak text sneak peak text v v vsneak peak text</BlogSneakPeakText>
 								</span>
 							</span>
 						</BlogCardContainer>
 						))}
 				</BlogCards>
-		</BlogContainer>
-		)
-}
-
-export default Blog;
+		</BlogContainer>*/}
